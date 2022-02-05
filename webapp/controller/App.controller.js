cc4ca3e602b2c108ded4e44238763239c15f5deb
Hugo <irgendwer@irgendwo.de>
2022-02-05 10:13:14 +0100

    mit i18n

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
-	"sap/ui/model/json/JSONModel"
- ], function (Controller,MessageToast,JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel"
+ ], function (Controller,MessageToast,JSONModel,ResourceModel) {
 	"use strict";
 	return Controller.extend("Quickstart.controller.App", {
 		onInit : function(){
@@ -11,9 +12,18 @@ sap.ui.define([
 			};
 			let oModel = new JSONModel(oData);
 			this.getView().setModel(oModel);
+			 // set i18n model on view
+			 var i18nModel = new ResourceModel({
+				bundleName: "Quickstart.i18n.i18n"
+			 });
+			 this.getView().setModel(i18nModel, "i18n");
 
 		},
 	   onShowHello : function () {
+        let oBundle = this.getView().getModel("i18n").getResourceBundle();
+		var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+		var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
 		MessageToast.show("Hello World");
 	   }
 	});
